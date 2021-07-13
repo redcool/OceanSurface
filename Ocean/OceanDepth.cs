53bfd14913a151cf674240b3f1f1e6025a1cdbaa
ken <redcool007@qq.com>
2021-07-13 15:16:39 +0800

    update OceanDepth.cs

--- Ocean/OceanDepth.cs
+++ Ocean/OceanDepth.cs
@@ -1,19 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
+[ExecuteAlways]
 public class OceanDepth : MonoBehaviour
 {
+    public RenderTexture colorRT;
+    public RenderTexture depthRT;
+    public RenderTexture depthTex;
     public Camera cam;
+
+    CommandBuffer blitBack, blitColor, blitDepth;
     // Start is called before the first frame update
     void Start()
     {
         if (!cam)
             cam = GetComponent<Camera>();
 
-        if (cam)
-            cam.depthTextureMode = DepthTextureMode.Depth;
+        if (!cam)
+            return;
+
+        InitBuffers(cam);
+        InitCommands();
+
+        // set targets
+        cam.SetTargetBuffers(colorRT.colorBuffer, depthRT.depthBuffer);
     }
 
+    private void OnDestroy()
+    {
+        ClearCommands();
+    }
+
+
+    void ClearCommand(Camera cam, CameraEvent e, CommandBuffer buf)
+    {
+        if (!cam || buf == null)
+            return;
+
+        cam.RemoveCommandBuffer(e, buf);
+        buf.Dispose();
+    }
+
+    private void ClearCommands()
+    {
+        ClearCommand(cam, CameraEvent.AfterEverything, blitBack);
+        ClearCommand(cam, CameraEvent.AfterEverything, blitDepth);
+    }
+
+    private void InitCommands()
+    {
+        blitBack = new CommandBuffer { name = "Blit Back" };
+        blitBack.Blit(colorRT, (RenderTexture)null);
+        cam.AddCommandBuffer(CameraEvent.AfterEverything, blitBack);
+
+
+        blitDepth = new CommandBuffer { name = "Blit Depth" };
+        blitDepth.Blit(depthRT.depthBuffer, depthTex.colorBuffer);
+        // bind textures
+        blitDepth.SetGlobalTexture("_CameraScreenTexture", colorRT);
+        blitDepth.SetGlobalTexture("_CameraDepthTexture", depthTex);
+        cam.AddCommandBuffer(CameraEvent.AfterEverything, blitDepth);
+    }
+
+    private void InitBuffers(Camera cam)
+    {
+        colorRT = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 0,RenderTextureFormat.ARGB32);
+        depthRT = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 24, RenderTextureFormat.Depth);
+        depthTex = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 0, RenderTextureFormat.R16);
+    }
+
+    //void OnPreRender()
+    //{
+    //    cam.SetTargetBuffers(colorRT.colorBuffer, depthRT.depthBuffer);
+    //}
 
 }
